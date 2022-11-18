7f2187699380600bb9a20b52e851173472c935af
krunal <krunal.maru1106@gmail.com>
2022-11-18 17:22:36 +0530

    category

--- myclothstore/home/models.py
+++ myclothstore/home/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Basemodel(models.Model):
+    uid = models.UUIDField(primary_key=True,editable=False,default=uuid.uuid4)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+class Category(Basemodel):
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
+
+class SubCategory(Basemodel):
+    name = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- myclothstore/home/views.py
+++ myclothstore/home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from home.models import Category,SubCategory
 # Create your views here.
+def home(request):
+    category = Category.objects.all()
+    context = {'category':category}
+    return render(request, 'home/index.html', context)
