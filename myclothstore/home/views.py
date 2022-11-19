dccc0a25dbb387e9de34d0e403f0e812b8510d01
krunal <krunal.maru1106@gmail.com>
2022-11-19 13:57:41 +0530

    product fetch

--- myclothstore/home/views.py
+++ myclothstore/home/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from home.models import Category,SubCategory
+from home.models import Category,SubCategory,Product
 # Create your views here.
 def home(request):
     category = Category.objects.all()
-    context = {'category':category}
+    categoryId = request.GET.get('category')
+    if categoryId:
+        product = Product.objects.filter(subcategory=categoryId).order_by('-uid')
+    else:
+        product = Product.objects.all()
+    context = {'category':category,'product':product}
     return render(request, 'home/index.html', context)
