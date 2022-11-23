1d57c9f14eb47a705aaedd4a5e68c689c38daa0e
krunal <krunal.maru1106@gmail.com>
2022-11-23 11:41:03 +0530

    login decorator

--- myclothstore/home/views.py
+++ myclothstore/home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from home.models import Category,SubCategory,Product
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 from home.models import UsercreateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -16,6 +17,7 @@ def home(request):
         product = Product.objects.all()
     context = {'category':category,'product':product}
     return render(request, 'home/index.html', context)
+
 
 def signup(request):
     if request.method == 'POST':
@@ -37,35 +39,41 @@ def signup(request):
     }
     return render(request, 'registration/signup.html',context)
 
+@login_required(login_url='/accounts/login/')
 def cart_add(request, id):
     cart = Cart(request)
     product = Product.objects.get(id = id)
     cart.add(product=product)
     return redirect('home')
 
+@login_required(login_url='/accounts/login/')
 def cart_remove(request):
     cart = Cart(request)
     cart.clear()
     return redirect('cart_detail')
 
+@login_required(login_url='/accounts/login/')
 def item_increase(request, id):
     cart = Cart(request)
     product = Product.objects.get(id = id)
     cart.add(product=product)
     return redirect('cart_detail')
 
-
+@login_required(login_url='/accounts/login/')
 def item_decrease(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.decrement(product=product)
     return redirect('cart_detail')
 
+
+@login_required(login_url='/accounts/login/')
 def item_clear(request, id):
     cart = Cart(request)
     product = Product.objects.get(id=id)
     cart.remove(product)
     return redirect('cart_detail')
 
+@login_required(login_url='/accounts/login/')
 def cart_detail(request):
     return render(request,'cart/cart_detail.html')
