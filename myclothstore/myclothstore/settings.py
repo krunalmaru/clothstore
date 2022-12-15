27fba308a5fbe2814af6f4f7b7c9fd51a5d0c6c0
krunal <krunal.maru1106@gmail.com>
2022-12-15 12:50:36 +0530

    sqlite3 to mysql

--- myclothstore/myclothstore/settings.py
+++ myclothstore/myclothstore/settings.py
@@ -86,10 +86,20 @@ WSGI_APPLICATION = 'myclothstore.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clothstore',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+  
+    
 }
 
 
@@ -142,7 +152,7 @@ EMAIL_HOST_USER = 'krunallm11@gmail.com'
 EMAIL_HOST_PASSWORD = 'rhbjdrjquozqtwcd'
 
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+# LOGOUT_REDIRECT_URL = 'home'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
